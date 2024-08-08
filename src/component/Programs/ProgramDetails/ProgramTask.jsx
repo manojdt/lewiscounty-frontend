04539664402688a9e21c1eb73651b5bc4cb07020
subramaniyant7 <tsubramaniyan2@gmail.com>
2024-08-08 14:15:29 +0530

    Fixes

--- src/component/Programs/ProgramDetails/ProgramTask.jsx
+++ src/component/Programs/ProgramDetails/ProgramTask.jsx
@@ -1,14 +1,19 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import DoubleArrowIcon from '../../../assets/images/double_arrow 1x.png';
 import ProgramVideo from '../../../assets/images/video.png';
 import ProgramDoc from '../../../assets/images/book.png';
 import SuccessTik from '../../../assets/images/blue_tik1x.png';
+import CancelIcon from '../../../assets/images/cancel-colour1x.png'
+import UploadIcon from "../../../assets/images/image_1x.png"
+import DeleteIcon from "../../../assets/images/delete_1x.png"
+import FileUploadIcon from "../../../assets/icons/Upload.svg"
 
 import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import { getProgramDetails, updateProgram } from '../../../services/userprograms';
 import { useDispatch, useSelector } from 'react-redux';
 import { Backdrop, CircularProgress } from '@mui/material';
 import { pipeUrls, programActionStatus } from '../../../utils/constant';
+import Tooltip from '../../../shared/Tooltip';
 
 export default function ProgramTask() {
     const navigate = useNavigate()
@@ -17,7 +22,10 @@ export default function ProgramTask() {
     const params = useParams();
     const { programdetails, loading: programLoading, error, status } = useSelector(state => state.userPrograms)
     const [loading, setLoading] = useState({ initial: true, join: false })
-    const [taskStage, setTaskStage] = useState('desc')
+    const [taskStage, setTaskStage] = useState('uploadfile')
+    const [logoImage, setLogoImage] = useState([])
+    const [fileNames, setFileNames] = useState([])
+    const fileRef = useRef(null);
 
     const handleTaskStage = (stage) => {
         if (stage === 'submit') {
@@ -33,16 +41,6 @@ export default function ProgramTask() {
         if (taskStage === 'docs') setTaskStage('video')
     }
 
-    // useEffect(() => {
-    //     if (taskStage === '') {
-    //         navigate(`${pipeUrls.assigntask}/${programdetails.id}`)
-    //         // setTimeout(() => {
-
-    //         // navigate(`${pipeUrls.assigntask}/${programdetails.id}`)
-    //         // }, [2000])
-    //     }
-    // }, [taskStage])
-
     useEffect(() => {
         if (Object.keys(programdetails).length) {
             setLoading({ ...loading, initial: false })
@@ -52,31 +50,63 @@ export default function ProgramTask() {
     useEffect(() => {
         if (status === programActionStatus.yettostart) {
             setTaskStage('submit')
-            // setTimeout(() => {
             setLoading({ initial: false, join: false })
             setTimeout(() => {
                 setTaskStage('')
                 navigate(`${pipeUrls.assigntask}/${programdetails.id}`)
             }, [2000])
-
-            // }, [2000])
         }
     }, [status])
 
 
     useEffect(() => {
-        console.log('searchParams', params)
-        // const programId = searchParams.get("id");
         const programId = params.id;
-        // if (programId === null) {
-        //   navigate(pipeUrls.programs)
-        // }
-
         if (programId && programId !== '') {
             dispatch(getProgramDetails(programId))
         }
 
     }, [params.id])
+
+    function readmultifiles(files, cb) {
+        let urls = [];
+        const reader = new FileReader();
+        function readFile(index) {
+            if (index >= files.length) return cb(urls);
+            const file = files[index];
+            reader.onload = function (e) {
+                const bin = e.target.result;
+                urls = [...urls, bin]
+                readFile(index + 1)
+            }
+            reader.readAsDataURL(file);
+        }
+        readFile(0);
+    }
+
+
+    const handleImageUpload = (e) => {
+        const allFiles = fileRef.current.files;
+        console.log('upload', allFiles, e.target.files[0].name)
+        setFileNames([...fileNames, e.target.files[0].name])
+        readmultifiles(allFiles, function (urls) {
+            setLogoImage(urls);
+        })
+    }
+
+    const handleDeleteImage = (i) => {
+        const removeImage = []
+        fileNames.forEach((file, index) => {
+            if (index !== i) removeImage.push(file)
+        })
+
+        console.log('fileNames', fileNames, i)
+        console.log('removeImage', removeImage, typeof i)
+        setFileNames(removeImage)
+    }
+
+    console.log('mmm', logoImage)
+    console.log('bbb', fileNames)
+
 
     return (
         <div className="px-9 my-6 grid">
@@ -87,8 +117,8 @@ export default function ProgramTask() {
                 <CircularProgress color="inherit" />
             </Backdrop>
             <div className='grid mb-10' style={{ boxShadow: '4px 4px 25px 0px rgba(0, 0, 0, 0.15)', borderRadius: '5px' }}>
-                <div className='breadcrum'>
-                    <nav className="flex px-7 pt-6 pb-5 mx-2 border-b-2" aria-label="Breadcrumb">
+                <div className='breadcrum '>
+                    <nav className="flex justify-between px-7 pt-6 pb-5 mx-2 border-b-2" aria-label="Breadcrumb">
                         <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
                             <li className="inline-flex items-center">
 
@@ -102,8 +132,8 @@ export default function ProgramTask() {
                             <li>
                                 <div className="flex items-center">
                                     <a href="#" className="ms-1 text-sm font-medium text-gray-700 hover:text-blue-600 md:ms-2 dark:text-gray-400 dark:hover:text-white">
-                                        Planned Program Joining details
-
+                                        {/* Planned Program Joining details */}
+                                        Upload Your Id Proof
                                         {taskStage === 'video' ? '- Video Introduction' : ''}
                                         {taskStage === 'docs' ? '- Case Study Introduction' : ''}
                                         {taskStage === 'question' ? '- Question and Answer' : ''}
@@ -114,6 +144,11 @@ export default function ProgramTask() {
                             </li>
 
                         </ol>
+                        <div className="flex gap-20 items-center">
+                            <Tooltip title="Cancel">
+                                <img className='cursor-pointer' onClick={() => navigate(`/program-details/${params.id}`)} src={CancelIcon} alt="CancelIcon" />
+                            </Tooltip>
+                        </div>
                     </nav>
                 </div>
                 <div className='content px-32 py-6'>
@@ -259,12 +294,95 @@ export default function ProgramTask() {
                                                 <div className='flex justify-center items-center flex-col gap-5 py-10 px-20 mt-20 mb-20'
                                                     style={{ background: 'linear-gradient(101.69deg, #1D5BBF -94.42%, #00AEBD 107.97%)', borderRadius: '10px' }}>
                                                     <img src={SuccessTik} alt="SuccessTik" />
-                                                    <p className='text-white text-[12px]'>You have selected for this program.</p>
+                                                    <p className='text-white text-[12px]'>
+                                                        
+                                                        {/* You have selected for this program. */}
+                                                        Your ID proof has been approved,& you have successfully joined.
+                                                        
+                                                        </p>
                                                 </div>
 
                                             </div>
 
-                                            : null
+                                            : taskStage === 'uploadfile' ?
+
+                                                <div className='py-6'>
+                                                    <p className='text-[14px] pb-10'>
+
+                                                        For the safety of mentees, we kindly request that you upload your ID proof. Ensuring the security and trustworthiness of our mentoring community is our top priority.
+                                                    </p>
+
+                                                    <div className="flex items-center justify-center w-full">
+                                                        <label htmlFor="dropzone-file"
+                                                            className="flex flex-col items-center justify-center w-full h-64 border-2
+                                                                                 border-gray-300 border-dashed cursor-pointer
+                                                                                   dark:hover:bg-bray-800  hover:bg-gray-100
+                                                                                   dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 input-bg">
+                                                            <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                                                <img src={FileUploadIcon} alt="FileUploadIcon" />
+                                                                <p className="mb-2 text-sm pt-5" style={{ color: '#C6C6C6' }}><span>
+                                                                    Upload Your  Id Proof (Png, Img,PDF)</span>
+                                                                </p>
+
+                                                            </div>
+                                                            <input id="dropzone-file" type="file"
+                                                                ref={fileRef}
+                                                                onChange={handleImageUpload
+
+                                                                    //     (e) => {
+
+                                                                    //     console.log(e)
+                                                                    //     if (e.target.files && e.target.files[0]) {
+                                                                    //         console.log(e.target.files[0])
+                                                                    //         let types = ['image/png', 'image/jpeg']
+                                                                    //         console.log(e.target.files[0].type)
+                                                                    //         if (types.includes(e.target.files[0].type)) {
+                                                                    //             setLogoImage(URL.createObjectURL(e.target.files[0]));
+                                                                    //         }
+                                                                    //     }
+                                                                    // }
+
+                                                                }
+                                                                className="hidden" />
+                                                        </label>
+
+                                                    </div>
+                                                    {fileNames.length ?
+                                                        <>
+                                                            <div className='flex gap-3'>
+                                                                {
+                                                                    fileNames.map((file, i) =>
+
+                                                                        <div className='flex justify-between items-center w-[30%] mt-5 px-4 py-4 input-bg'
+                                                                            style={{ border: '1px solid rgba(29, 91, 191, 0.5)', borderRadius: '3px' }}>
+                                                                            <div className='flex w-[80%] gap-3 items-center'>
+                                                                                <span className='text-[12px]'> {file}</span>
+                                                                            </div>
+                                                                            <img className='w-[30px] cursor-pointer' onClick={() => handleDeleteImage(i)} src={DeleteIcon} alt="DeleteIcon" />
+                                                                        </div>
+                                                                    )
+                                                                }
+
+                                                            </div>
+                                                            <div className='flex justify-center items-center gap-6 pt-16'>
+                                                                <button className='py-2 px-9 text-[12px] flex items-center' style={{ border: '1px solid rgba(29, 91, 191, 1)', borderRadius: '28px' }}
+                                                                    onClick={() => navigate(`/program-details/${programdetails.id}`)}
+                                                                >Cancel
+                                                                </button>
+
+                                                                <button className='py-2 px-9 text-white text-[12px] flex items-center' style={{
+                                                                    background: "linear-gradient(94.18deg, #00AEBD -38.75%, #1D5BBF 195.51%)",
+                                                                    borderRadius: '28px'
+                                                                }}
+                                                                    onClick={() => handleTaskStage('submit')}
+                                                                >Submit
+                                                                </button>
+                                                            </div>
+                                                        </>
+                                                        : null
+                                                    }
+                                                </div>
+                                                : null
                     }
 
                 </div>
