5549a66509c9fdd9ee3c07953013fab1fa5b7617
subramaniyant7 <tsubramaniyan2@gmail.com>
2024-10-11 10:36:37 +0530

    Search Functionality

--- src/shared/Navbar/index.jsx
+++ src/shared/Navbar/index.jsx
@@ -18,23 +18,38 @@ import { Backdrop } from '@mui/material';
 import { Button } from '../Button';
 
 import { OverlayPanel } from 'primereact/overlaypanel';
-import NotificationMenu from '../../component/Notification/NotificationMenu';
 import Notification from '../../component/Notification';
 import { userActivities } from '../../services/activities';
+import api from '../../services/api';
 
 export const Navbar = () => {
     const navigate = useNavigate();
-    const moreMenu = useRef(null);
+    const searchBar = useRef(null);
     const op = useRef(null);
-    const [showNotification, setShowNotification] = useState(false)
     const [isLogout, setIsLogout] = useState(false)
     const userInfo = useSelector(state => state.userInfo)
     const location = useLocation();
     const dispatch = useDispatch();
     const [anchorEl, setAnchorEl] = useState(null);
+    const [searchProps, setSearchProps] = useState({ searchType: 'program', search: '', dropOpen: false, searchData: [], current: null })
     const open = Boolean(anchorEl);
     const { pathname } = location
     const role = userInfo.data.role || ''
+
+    const filterBtn = [
+        {
+            key: 'program',
+            name: 'Program'
+        },
+        {
+            key: 'mentor',
+            name: 'Mentors'
+        },
+        {
+            key: 'mentee',
+            name: 'Mentees'
+        },
+    ]
 
 
     const handleClick = (event) => {
@@ -95,6 +110,39 @@ export const Navbar = () => {
         document.getElementsByClassName('notification-image')[0].click()
     }
 
+    const handleGlobalChange = async (value) => {
+        const getSearchData = await api.get(
+            `globalsearch?keyword=${searchProps.searchType}&search=${value}`
+        );
+        if (getSearchData.status === 200 && getSearchData.data) {
+            setSearchProps({ ...searchProps, searchData: getSearchData.data });
+        }
+    }
+
+    const handleSelectFilter = (key) => {
+        setSearchProps({ ...searchProps, searchType: key, searchData: [] })
+    }
+
+
+    const handleOpenSearchBar = e => {
+        if (!document.getElementById('search_overlay_panel')) {
+            searchBar.current.toggle(e)
+        }
+        setSearchProps({ ...searchProps, searchData: [], dropOpen: true, current: e })
+    }
+
+    const searchNavigation = (url) => {
+        searchBar.current.toggle(searchProps.current)
+        document.getElementById('search-navbar').value = ''
+        setSearchProps({...searchProps, searchData: [], searchType: 'program', current: null})
+        navigate(url)
+    }
+
+    useEffect(() => {
+        console.log('Current', searchBar.current)
+    },[searchBar.current])
+
+
 
     useEffect(() => {
         dispatch(userActivities())
@@ -130,23 +178,6 @@ export const Navbar = () => {
                 <div className='flex justify-between'>
                     <div className="contain flex justify-between w-3/12 p-4">
                         <div className="site-logo cursor-pointer flex items-center space-x-3 rtl:space-x-reverse" onClick={() => navigate('/dashboard')}>
-                            {/* <svg
-                                width="40"
-                                height="35"
-                                viewBox="0 0 59 59"
-                                fill="none"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    clipRule="evenodd"
-                                    d="M29.0588 58.5365C45.1076 58.5365 58.1177 45.4327 58.1177 29.2682C58.1177 13.1039 45.1076 0 29.0588 0C13.0101 0 0 13.1039 0 29.2682C0 45.4327 13.0101 58.5365 29.0588 58.5365ZM38.1242 13.6344C38.5654 12.0557 37.0444 11.1221 35.6552 12.119L16.2629 26.0335C14.7564 27.1145 14.9934 29.2682 16.6189 29.2682H21.7254V29.2284H31.6778L23.5685 32.1103L19.9935 44.9022C19.5523 46.4809 21.0732 47.4144 22.4625 46.4176L41.8548 32.5031C43.3613 31.4221 43.1242 29.2682 41.4988 29.2682H33.7549L38.1242 13.6344Z"
-                                    fill="#00AEBD"
-                                    style={{
-                                        fill: "#00AEBD;fill:color(display-p3 0.0000 0.6824 0.7412);fill-opacity:1",
-                                    }}
-                                />
-                            </svg> */}
                             <span className="self-center text-2xl font-semibold whitespace-nowrap">My Logo</span>
                         </div>
                         <div className='navbar-mobile-menu' onClick={handleLeftSidebar}>
@@ -172,10 +203,37 @@ export const Navbar = () => {
                     <div className={`navbar-icons flex items-center justify-between ${getWindowDimensions().width <= 1536 ? 'w-3/6' : 'w-2/5'} p-4`}>
                         <div className="relative mt-1 search-container">
                             <input type="text" id="search-navbar" className="block w-full p-2 text-sm text-gray-900 border-none rounded-lg"
-                                placeholder="Search..." style={{ backgroundColor: '#F5F9FF', width: '430px', height: '50px', borderRadius: '3px' }} />
+                                placeholder="Search..." style={{ backgroundColor: '#F5F9FF', width: '430px', height: '50px', borderRadius: '3px' }}
+                                onClick={(e) => handleOpenSearchBar(e)}
+                                onChange={(e) => handleGlobalChange(e.target.value)}
+                            />
                             <div className="absolute inset-y-0 end-0 flex items-center pe-3 pointer-events-none">
                                 <img src={SearchIcon} alt="SearchIcon" />
                             </div>
+
+                            <OverlayPanel ref={searchBar} id="search_overlay_panel" style={{ width: '430px', top: '63px !important' }} 
+                                className="notification-container searchbar-container" onClose={() => console.log('Close')}>
+                                <div className='flex gap-4'>
+                                    {
+                                        filterBtn.map(fBtn =>
+                                            <button key={fBtn.key} onClick={() => handleSelectFilter(fBtn.key)}
+                                                className={`${searchProps.searchType === fBtn.key ? 'active-info' : ''}`}>{fBtn.name}</button>)
+                                    }
+                                </div>
+                                <div>
+                                    <ul>
+                                        {
+                                            searchProps.searchData.map((sData, i) => {
+                                                let name = searchProps.searchType === 'program' ? sData.program_name : sData.name
+                                                let url = searchProps.searchType === 'program' ? `program-details/${sData.id}` : `mentor-details/${sData.id}`
+                                                return <li key={i} className='cursor-pointer' onClick={() => searchNavigation(url)}>
+                                                    {name}
+                                                </li>
+                                            })
+                                        }
+                                    </ul>
+                                </div>
+                            </OverlayPanel>
                         </div>
                         <img className='search-icon hidden' src={SearchIcon} alt="SearchIcon" />
                         <div className='relative notitification-group'>
