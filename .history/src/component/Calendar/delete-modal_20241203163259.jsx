3a6bf12c4ec8ca4ce2b9f9faa0eac022813a7d61
Park Avan <ipark.avan@gmail.com>
2024-12-04 09:00:55 +0530

    delete history

--- .history/src/component/Calendar/delete-modal_20241203163259.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import MuiModal from '../../shared/Modal';
-
-const DeleteModal = () => {
-  return <MuiModal noheader>DeleteModal</MuiModal>;
-};
-
-export default DeleteModal;
