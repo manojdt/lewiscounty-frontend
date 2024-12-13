adffa40dd8bd91b292c3081061e78ff9ac5a090a
Balaji S R <balajis@dataterrain.com>
2024-12-14 00:28:46 +0530

    inprogress

--- src/component/testimonialView/index.jsx
+++ src/component/testimonialView/index.jsx
@@ -1,10 +1,11 @@
-import { Box, Divider, Typography } from '@mui/material'
+import { Box, Divider, Stack, Typography } from '@mui/material'
 import React from 'react'
+import { Button } from '../../shared'
 
 export const TestimonialView = () => {
     return (
-        <Box>
-            <Typography>Tesimonial Name</Typography>
+        <Box className="!border !border-[#DBE0E5] rounded-[10px] m-[20px] p-[20px]">
+            <Typography p={"20px 0px"}>Tesimonial Name</Typography>
             <Divider></Divider>
 
             <div className="relative flex gap-6 justify-between">
@@ -15,7 +16,7 @@ export const TestimonialView = () => {
                                 Category
                             </th>
                             <td className="px-6 py-4 text-white" style={{ background: 'rgba(0, 174, 189, 1)' }}>
-                                
+
                             </td>
                         </tr>
                         <tr className="bg-white border-b">
@@ -23,7 +24,7 @@ export const TestimonialView = () => {
                                 Program Name
                             </th>
                             <td className="px-6 py-4 text-white" style={{ background: 'rgba(0, 174, 189, 1)' }}>
-                                
+
                             </td>
                         </tr>
                         <tr className="bg-white border-b">
@@ -39,7 +40,7 @@ export const TestimonialView = () => {
                                 Mentor Name
                             </th>
                             <td className="px-6 py-4 text-white" style={{ background: 'rgba(0, 174, 189, 1)' }}>
-                                
+
                             </td>
                         </tr>
 
@@ -53,7 +54,7 @@ export const TestimonialView = () => {
                                 Program Start Date and Time
                             </th>
                             <td className="px-6 py-4 text-white" style={{ background: 'rgba(29, 91, 191, 1)' }}>
-                                
+
                             </td>
                         </tr>
                         <tr className="bg-white border-b">
@@ -61,7 +62,7 @@ export const TestimonialView = () => {
                                 Program End Date and Time
                             </th>
                             <td className="px-6 py-4 text-white" style={{ background: 'rgba(29, 91, 191, 1)' }}>
-                                
+
                             </td>
                         </tr>
                         <tr className="bg-white border-b">
@@ -79,17 +80,38 @@ export const TestimonialView = () => {
             </div>
 
 
-            <Box className="!border !border-[#1D5BBF80] p-[20px] rounded-[3px]">
+            <Box className="!border !border-[#1D5BBF80] p-[20px] rounded-[3px]" mt={3}>
                 <Typography className='!text-[#1D5BBF] !text-[14px]'>
                     Description: <span className='!text-[#18283D] !text-[14px]'>sdfsdfsfsdfsffsdf</span>
                 </Typography>
             </Box>
 
 
-            <Box className="bg-[#1D5BBF0D] rounded-[3px] border border-[#1D5BBF80]">
+            <Box className="bg-[#1D5BBF0D] rounded-[3px] border border-[#1D5BBF80]" mt={3}>
                 <Typography className='px-[20px] py-[12px] border-b-2 border-[#1D5BBF80] !text-[#1D5BBF] !text-[16px]'>Title</Typography>
                 <Typography className='px-[20px] py-[12px] !text-[#1D5BBF] !text-[16px]'>Title</Typography>
             </Box>
+
+
+            <Stack direction="row" alignItems="center" justifyContent="center" width="100%" mt={3}>
+                <Stack direction={"row"} alignItems={"center"} spacing={"20px"}>
+                    <Button
+                        btnType='button'
+                        btnCategory='secondary'
+                        btnName="Close"
+                        btnCls='!border !border-[#18283D] !text-[#18283D]' />
+                    <Button
+                        btnType='button'
+                        btnCategory='secondary'
+                        btnName="Cancel"
+                        btnCls='!border !border-[#FFE7E7] !text-[#E0382D] !bg-[#FFE7E7]' />
+                    <Button
+                        btnType='button'
+                        btnCategory='primary'
+                        btnName="Accept"
+                        btnCls='' />
+                </Stack>
+            </Stack>
         </Box>
     )
 }
