e62bf8c3bae82b6751b59e1a393376c7a90c65fd
Park Avan <ipark.avan@gmail.com>
2024-12-19 22:46:39 +0530

    bug fix

--- src/shared/paymentButton/checkout-form.jsx
+++ src/shared/paymentButton/checkout-form.jsx
@@ -23,7 +23,7 @@ const CheckoutForm = () => {
       //`Elements` instance that was used to create the Payment Element
       elements,
       confirmParams: {
-        return_url: 'http://localhost:3000/tickets',
+        return_url: 'http://localhost:3000/dashboard',
       },
     });
 
